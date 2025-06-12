f9bacad23fb3a7edf6385c462782c98ab6de2a1a
Catalin Jumiga <catalinjumiga4@gmail.com>
2025-06-12 16:32:19 +0300

    updated programs to use api types

--- programs/environment/src/state/environment.rs
+++ programs/environment/src/state/environment.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use api::types::overlock::crossplane::v1beta1;
+use std::str::FromStr;
 
 #[account]
 #[derive(InitSpace)]
@@ -7,4 +9,24 @@ pub struct Environment {
     pub name: String,
     pub owner: Pubkey,
     pub provider: Pubkey,
+}
+
+impl From<v1beta1::Environment> for Environment {
+    fn from(proto: v1beta1::Environment) -> Self {
+        Environment {
+            name: proto
+                .metadata
+                .as_ref()
+                .map(|m| m.name.clone())
+                .unwrap_or_default(),
+
+            owner: Pubkey::from_str(&proto.creator).unwrap_or(Pubkey::default()),
+
+            provider: {
+                let mut bytes = [0u8; 32];
+                bytes[24..].copy_from_slice(&proto.provider.to_be_bytes());
+                Pubkey::new_from_array(bytes)
+            },
+        }
+    }
 }
--- programs/provider/src/state/provider.rs
+++ programs/provider/src/state/provider.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use api::types::overlock::crossplane::v1beta1;
 
 #[account]
 #[derive(InitSpace)]
@@ -13,4 +14,22 @@ pub struct Provider {
     #[max_len(50)]
     pub environment_type: String,
     pub availability: bool,
+}
+
+impl From<v1beta1::Provider> for Provider {
+    fn from(proto: v1beta1::Provider) -> Self {
+        Provider {
+            name: proto
+                .metadata
+                .as_ref()
+                .map(|m| m.name.clone())
+                .unwrap_or_default(),
+            ip: proto.ip,
+            port: proto.port as u16,
+            country: proto.country_code,
+            environment_type: proto.environment_type,
+            availability: proto.availability.to_lowercase() == "true"
+                || proto.availability.to_lowercase() == "available",
+        }
+    }
 }
