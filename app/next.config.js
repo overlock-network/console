c0b795523c5af0f8b30c7a4f0b977432ebc1d18f
ionition <vlad.ionita25.04@gmail.com>
2025-06-25 17:15:00 +0300

    enable auto deploy of Console to GitHub pages on version tag creation

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "https://console.overlock.network",
   reactStrictMode: true,
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
