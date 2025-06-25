a0bc72bb02caff15b55424a7e1ffeb68bd67a577
ionition <vlad.ionita25.04@gmail.com>
2025-06-25 14:47:50 +0300

    allow to build console application as static pages (#33)

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
