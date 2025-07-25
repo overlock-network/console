c94344837d54a7b6c90af71b1b383ad60d173127
ionition <vlad.ionita25.04@gmail.com>
2025-07-25 17:21:58 +0300

    removed address column from collections table (#90)

--- app/src/components/ListTable/ContractInfoColumns.tsx
+++ app/src/components/ListTable/ContractInfoColumns.tsx
@@ -3,16 +3,8 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "./ColumnHeader";
 import { Contract } from "@/lib/types";
-import { formatAddress } from "@/lib/utils";
 
 export const ContractInfoColumns = (): ColumnDef<Contract>[] => [
-  {
-    accessorKey: "address",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Address" />
-    ),
-    cell: ({ row }) => formatAddress(row.original.address),
-  },
   {
     accessorKey: "name",
     header: ({ column }) => (
